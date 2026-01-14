645fd92c148c6050a33bde317ce3f2a9b3140ff3
anthony <ferrere-hoar@insa-toulouse.fr>
2026-01-14 17:46:24 +0100

    commit

--- src/FordF.ml
+++ src/FordF.ml
@@ -59,12 +59,12 @@ let rec send_info (gr: string graph) (id_list: id list) (msg_size: int) =
       | None -> gr
       | Some arc -> let new_lbl = ((int_of_string arc.lbl) - msg_size) in
         let updated_arc = { src = x; tgt = y; lbl = (string_of_int new_lbl) } in new_arc gr updated_arc ) in
-    let gr_retour = (
+    let gr_back = (
       match find_arc gr_flow y x with
-        | Some arc_retour -> let new_flow = ((int_of_string arc_back.lbl) + msg_size ) in
+        | Some arc_back -> let new_flow = ((int_of_string arc_back.lbl) + msg_size ) in
           let updated_back = { src = y; tgt = x; lbl = string_of_int new_flow } in new_arc gr_flow updated_back
         | None -> let back_arc = { src = y; tgt = x; lbl = string_of_int msg_size } in new_arc gr_flow back_arc) in
-      send_info gr_retour (y :: rest) msg_size
+      send_info gr_back (y :: rest) msg_size
 
 
   let rec ffalgo (gr: string graph) (s: id) (e: id) =
