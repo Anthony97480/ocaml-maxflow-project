0875a218006b736a79322794f8da84cfb53248c5
tony <ferrere-hoar@insa-toulouse.fr>
2026-01-15 13:54:05 +0100

    erreur

--- ocaml-maxflow-project-main/src/FordF1.mli
+++
@@ -1,9 +0,0 @@
-open Graph
-
-
-
-val find_path: string graph -> id -> id -> string arc list -> id list -> id list option
-
-val max_sending_flow: string graph -> id list -> id -> id
-
-val ffalgo: string graph -> id -> id -> string graph
